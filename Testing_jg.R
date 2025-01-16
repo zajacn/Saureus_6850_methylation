675e3bd658251e42fb363f2e5d2ece316296a7b2
Jonas Grossmann <jonas.grossmann@fgcz.uzh.ch>
2025-01-16 13:14:50 +0100

    try to look into it

--- Testing_jg.R
+++ Testing_jg.R
@@ -6,10 +6,20 @@
 #
 
 # Jonas looks into methylation data
+library(tidyverse)
+
 
 mtx <- readRDS("resources/methylation_data.rds")
 dim(mtx)
 
 
-# try to visualize
+# try to visualize mA6 as binary heatmap along the genome
+str(mtx)
+
+mtx |> select(c(treatment, group, strand))
+
+
+  as("data.frame") |>
+  as.matrix() |>
+  pheatmap::pheatmap()
 
